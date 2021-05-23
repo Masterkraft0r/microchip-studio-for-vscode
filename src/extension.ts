5d2ba5f801816805bda7a19748a87d38dd4e7ebf
Benjamin Bugl <benjamin.bugl@gmx.net>
2021-05-23 16:17:34 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,53 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+const vsparse = require("vs-parse");
+
+async function importSolution(solutionFile: vscode.Uri) {
+	let solution = await vsparse.parseSolution(solutionFile.fsPath);
+	// let basePath = 
+	console.log(solutionFile.fsPath);
+	console.log(solution);
+	// TODO: create workspace object
+	// let workspaceFile = vscode.window.showSaveDialog();
+}
+
+async function addProject(projectFile: vscode.Uri) {
+	let project = await vsparse.parseProject(projectFile.fsPath);
+	// let basePath = 
+	console.log(project);
+}
+
+export async function activate(context: vscode.ExtensionContext) {
+	console.log('Atmel Studio Importer is now active!');
+	let inputFiles = await vscode.window.showOpenDialog({canSelectFiles: true, canSelectFolders: false, canSelectMany: false});
+
+	if (inputFiles === undefined) {
+		throw new vscode.FileSystemError("No file has been chosen.");
+	}
+
+	let inputFile = inputFiles[0]; 
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('atmel-studio-importer.import-solution',
+			() => {
+				importSolution(inputFile);
+				// TODO: create workspace object
+				// let workspaceFile = vscode.window.showSaveDialog();
+			}
+		)
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('atmel-studio-importer.add-project',
+			() => {
+				addProject(inputFile);
+				// TODO: check if workspace is open
+				// TODO: check if file is project
+				// TODO: parse project
+				// TODO: add folder to workspace
+			}
+		)
+	);
+}
+
+export function deactivate() {}
